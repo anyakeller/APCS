9209c17e12d1f38927b23466ebc28ec28891ab50
Anya Keller <anyaisanya@gmail.com>
2015-09-16 16:45:44 -0400

    First commit for this repo

---
+++ MinChange.java
@@ -0,0 +1,25 @@
+public class MinChange{
+    public static void main(String[] args){
+        System.out.println("Here is your change ");
+        int coins = Integer.parseInt(args[0]);
+        int q = 0;
+        int d = 0;
+        int n = 0;
+        int p = 0;
+        int change;
+        
+        
+        q = coins / 25;
+        coins = coins % 25;
+        d = coins / 10;
+        coins = coins % 10;
+        n = coins / 5;
+        coins = coins % 5;
+        p = coins;
+
+        System.out.println("quarters: " +q);
+        System.out.println("dimes: " +d);
+        System.out.println("nickels: " +n);
+        System.out.println("pennies: " +p);
+    }
+}

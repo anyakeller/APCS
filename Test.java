55f31fd0e9fb51db6b99aa0b32c5bf25cf4bfb89
Anya Keller <anyaisanya@gmail.com>
2015-09-21 13:35:49 -0400

    asdf

--- Test.java
+++ Test.java
@@ -3,11 +3,6 @@ public class Test{
 	int a =Integer.MAX_VALUE;
 
 	System.out.println(a);
-	System.out.println(a+1);
-	System.out.println(2-a);
-	System.out.println(-2-a);
-	System.out.println(2*a);
-	System.out.println(4*a);
 
     }
 }

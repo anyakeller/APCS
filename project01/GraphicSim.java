67bfcea0041626af0d9b79b6249059aa5a73871d
Anya Keller <anyaisanya@gmail.com>
2015-11-06 09:03:58 -0500

    added GraphicSim.java (probelm 19)

---
+++ project01/GraphicSim.java
@@ -0,0 +1,17 @@
+public class GraphicSim{
+    
+    public static void main(String[] args) {
+        StdDraw.setPenColor(StdDraw.RED);
+        int N = StdIn.readInt();
+        double d = StdIn.readDouble();
+        
+        for (int i = 0; i < N; i++){
+            for (int k = 0; k < N; k++){
+                double r = (1/d) * Math.sqrt((StdIn.readDouble()) / (Math.PI));
+                double x =  0.5 * (1.0/d) + (1/d) * k;
+                double y =  0.5 * (1.0/d) + (1/d) * i;
+                StdDraw.filledCircle(x, y, r);
+            }
+        }
+    }
+}
